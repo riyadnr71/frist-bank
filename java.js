341a17bf6dbe749b79f0fcaccc73f09801116109
riyadnr71 <riyadprodhan91@gmail.com>
2025-08-23 00:17:51 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,30 @@
+// login-btn"
+
+// console.log()
+
+
+
+document.getElementById('login-btn')
+.addEventListener('click', function(e){
+    e.preventDefault()
+   const mobileNumber = 17857111911;
+   const pinNumber = 1234;
+   const mobileNumberValue = document.getElementById("mobile-number").value
+
+    const mobileNumberValueCanverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+
+    const pinNumberValueCanverted = parseInt(pinNumberValue)
+    console.log(mobileNumberValueCanverted, pinNumberValueCanverted)
+
+    if(mobileNumberValueCanverted === mobileNumber && pinNumberValueCanverted === pinNumber){
+        window.location.href="./Home.html"
+
+    }
+    else{
+        alert('Invalid')
+    }
+
+
+})
